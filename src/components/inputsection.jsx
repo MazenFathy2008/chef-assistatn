230938e7c1d98828ab3e31eea57ad8402c333b77
Mazen Fathy <mazenfathy841@gmail.com>
2026-05-17 18:02:28 +0300

    preparing ingredients list and saving it in local sorage

--- src/components/inputsection.jsx
+++ src/components/inputsection.jsx
@@ -1,18 +1,31 @@
+import data from "../data.js"
 export default function InputSection(){
+    function handelSubmit(event){
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIng= formData.get("ing-in")
+        if(newIng){
+            event.currentTarget.reset()
+            data.push(newIng)
+            localStorage.setItem("ing",JSON.stringify(data))
+        }
+    }
+    const listOfIng = data.map(ing=><li>{ing}</li>)
     return(
         <section className="input-Sec">
-            <div className="input-f">
-                <input type="text" placeholder="e.g orange"/>
+            <form className="input-f" onSubmit={(event)=>{handelSubmit(event)}}>
+                <input type="text" placeholder="e.g orange" name="ing-in"/>
                 <button>+ Add ingredient</button>
-            </div>
-            <div className="ingredients-con">
+            </form>
+            {data.length>0?<div className="ingredients-con">
                 <h1>Ingredients on hand:  </h1>
                 <ul>
-                <li>orange</li>
-                <li>Annas</li>
-                <li>milk</li>
+                {listOfIng}
                 </ul>
             </div>
+            :
+            <h1 className="empty-list">There is no ingredients 🥱</h1>}
+            
             <div className="ready-con">
                 <div>
                     <span>Ready for a recipe?</span>
---
+++ src/data.js
@@ -0,0 +1,2 @@
+const data = JSON.parse(localStorage.getItem("ing"))||[];
+export default data;
