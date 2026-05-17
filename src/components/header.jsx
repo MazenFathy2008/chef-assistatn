4a6bad9e1aa33f35808d7cec24c77d7f24501604
Mazen Fathy <mazenfathy841@gmail.com>
2026-05-17 15:21:45 +0300

    header creating and styling

---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+import "../styles/header.css"
+export default function Header(){
+    return(
+        <header>
+            <img src="chef-icon.png"/>
+            <h1>Chef Assistant</h1>
+        </header>
+    )
+}
